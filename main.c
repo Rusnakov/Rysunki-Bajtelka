08112047d15b5cecb51f51d744b9513a9b5cc4bf
Rusnakov <tomasz.rusnak@gmail.com>
2022-11-01 22:16:49 +0100

    Końcowy, poprawny działający kod!

--- main.c
+++ main.c
@@ -1,166 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*#define BUFSIZE 1000000
-int wektor_wierzcholkow_pola[BUFSIZE], dlugosc_wektora_wierzcholkow_pola;
-long long int pole_zadruku_czarne=0, pole_zadruku_szare=0;
-void bufClear (char *buf)
-{
-    int i;
-    for (i=0; i<BUFSIZE; i++)
-    {
-        buf[i]='\0';
-    }
-}
-void clear_buf_string (char *buf)
-{
-    int i;
-    for (i=0; i<8; i++)
-    {
-        buf[i]=' ';
-    }
-}
-int dlugosc_slowa_w_buforze (char *wektor_danych)
-{
-    int i;
-    if (*wektor_danych)
-    {
-        for (i=0; i<BUFSIZE; i++)
-        {
-            if (wektor_danych[i]=='\n' || wektor_danych[i]=='\0') return i;
-        }
-    }
-    return -1;
-}
-void zamianaSlowaNaLiczy (char *wektor_danych)
-{
-    int iloscCyfrWLiczbie=8;
-    int i, licznikLiczbyIntowej=0, l, licznik_cyfry_liczby_w_stringu=iloscCyfrWLiczbie;
-    char buforNaLiczbeWSringu[]= {"        "};
-    l = dlugosc_slowa_w_buforze(wektor_danych);
-    for (i=l-1; i>-1; i--)
-    {
-        if (wektor_danych[i]!=0x20 )
-        {
-            buforNaLiczbeWSringu[licznik_cyfry_liczby_w_stringu]=wektor_danych[i];
-            licznik_cyfry_liczby_w_stringu--;
-            if (i==0)
-            {
-                wektor_wierzcholkow_pola[licznikLiczbyIntowej]=atoi(buforNaLiczbeWSringu);
-                //printf("Liczba wpisana w wektor = %i\n",wektor_wierzcholkow_pola[licznikLiczbyIntowej]);
-                licznikLiczbyIntowej++;
-                licznik_cyfry_liczby_w_stringu=iloscCyfrWLiczbie;
-                clear_buf_string(buforNaLiczbeWSringu);
-            }
-        }
-        else if (wektor_danych[i]==0x20 && wektor_danych[i-1]!=0x20)//// flage o spacje
-        {
-            wektor_wierzcholkow_pola[licznikLiczbyIntowej]=atoi(buforNaLiczbeWSringu);
-            //printf("Liczba wpisana w wektor = %i\n",wektor_wierzcholkow_pola[licznikLiczbyIntowej]);
-            licznikLiczbyIntowej++;
-            licznik_cyfry_liczby_w_stringu=iloscCyfrWLiczbie;
-            clear_buf_string(buforNaLiczbeWSringu);
-        }
-    }
-    dlugosc_wektora_wierzcholkow_pola=licznikLiczbyIntowej;
-}*/
 
-char wczytanie_wiersza_danych (void)
+long long int wczytanie_wiersza_danych ()
 {
-    int bufor[2][100000];
-    int licznik_wczytywanego_wierzcholka=0;
-    scanf("%d", &bufor[0][0]);
-    scanf("%d", &bufor[1][0]);
-    do
-    {
-        licznik_wczytywanego_wierzcholka++;
-        scanf("%d", &bufor[0][licznik_wczytywanego_wierzcholka]);
-        scanf("%d", &bufor[1][licznik_wczytywanego_wierzcholka]);
+    long long int pole=0, bufor[2][100000]={0}, p1, p2;
+    int ilosc_wierzcholkow=0;
+    scanf("%I64u", &bufor[0][0]);
+    scanf("%I64u", &bufor[1][0]);
+
+    do    {
+        ilosc_wierzcholkow++;
+        scanf("%I64u", &bufor[0][ilosc_wierzcholkow]);
+        scanf("%I64u", &bufor[1][ilosc_wierzcholkow]);
     }
-    while ((bufor[0][0]!=bufor[0][licznik_wczytywanego_wierzcholka]) || (bufor[1][0]!=bufor[1][licznik_wczytywanego_wierzcholka]));
-    return &bufor[0][0];
+    while ((bufor[0][0]!=bufor[0][ilosc_wierzcholkow]) || (bufor[1][0]!=bufor[1][ilosc_wierzcholkow]));
+
+    for (int i=0; i<ilosc_wierzcholkow; i++)    {
+        p1=bufor[0][i]*bufor[1][i+1];
+        p2=bufor[0][i+1]*bufor[1][i];
+        pole = (pole+(p1-p2));
+    }
+
+    if (pole < 0){
+        pole = pole*(-1);
+    };
+
+    return pole;
 }
 
-int main()
-{
-    int ilosc_testow, bufor[0][0];
+int main() {
+
+    int ilosc_testow;
     scanf("%d", &ilosc_testow);
-    for (int i=0; i<ilosc_testow; i++)
-    {
-        *bufor = wczytanie_wiersza_danych;
+
+    for (int i=0; i<ilosc_testow; i++) {
+        long long int pole1=0, pole2=0, tuszokule=0;
+        pole1 = wczytanie_wiersza_danych();
+        pole2 = wczytanie_wiersza_danych();
+
+        if (pole1 < pole2) {
+						tuszokule = (pole2-pole1)*3+pole1*5;
+        }
+
+        else {
+						tuszokule = (pole1-pole2)*3+pole2*5;
+        }
+
+        printf ("%I64u\n",tuszokule);
     }
-    for (int i=0; i<2; i++)
-    {
-        for (int j=0; j<=licznik_wczytywanego_wierzcholka; j++)
-        {
-            printf ("%i ", bufor[i][j]);
-        }
-    }
-
-    /*
-    int i, j, k, l, m, t;
-    long long int tuszokule, pole1, pole2, true_pole_szare, true_pole_czarne;
-    char buforSz[BUFSIZE]= {0};
-    char buforCz[BUFSIZE]= {0};
-    scanf("%i", &t);			                                // wczytanie ilo�ci zestaw�w danych
-    for (i=0; i<t; i++)
-    {
-        k = fgetc(stdin);                                       // zczytanie znaku pustej linii mi�dzy danymi (a w pierwszym przebiegu pozostawionego przez funkcj� scanf)
-        //printChar (k);		                                // wy�wietlenie znaku specjalnego
-        for (j=0; j<2; j++)
-        {
-            if (j==0) fgets(buforCz, sizeof buforCz, stdin);
-            if (j==1) fgets(buforSz, sizeof buforSz, stdin);
-        }
-        zamianaSlowaNaLiczy (buforCz);
-        for (k=1; k<=(dlugosc_wektora_wierzcholkow_pola-2)/2; k++)
-        {
-            int y1, x0, x2;
-            y1=dlugosc_wektora_wierzcholkow_pola-2*k;
-            x2=dlugosc_wektora_wierzcholkow_pola-2*k-1;
-            x0=dlugosc_wektora_wierzcholkow_pola-2*k+3;
-            if ( x0>dlugosc_wektora_wierzcholkow_pola) x0=-2*k+5;
-            pole_zadruku_czarne += (long long) wektor_wierzcholkow_pola[y1]*(wektor_wierzcholkow_pola[x2]-wektor_wierzcholkow_pola[x0]);
-    //            printf("Licznik k = %i, Indeksy:   y1 = %i, x2 = %i, x0 = %i\n",k,y1,x2,x0);
-    //            printf("                Wartosci:  %i,      %i,      %i\n",wektor_wierzcholkow_pola[y1],wektor_wierzcholkow_pola[x2],wektor_wierzcholkow_pola[x0]);
-    //            printf("Pole zadruku czarne = %i\n",pole_zadruku_czarne);
-        }
-        bufClear(wektor_wierzcholkow_pola);
-        dlugosc_wektora_wierzcholkow_pola=0;
-        zamianaSlowaNaLiczy (buforSz);
-        for (k=1; k<=(dlugosc_wektora_wierzcholkow_pola-2)/2; k++)
-        {
-            int y1, x0, x2;
-            y1=dlugosc_wektora_wierzcholkow_pola-2*k;
-            x2=dlugosc_wektora_wierzcholkow_pola-2*k-1;
-            x0=dlugosc_wektora_wierzcholkow_pola-2*k+3;
-            if ( x0>dlugosc_wektora_wierzcholkow_pola) x0=-2*k+5;
-            pole_zadruku_szare += (long long) wektor_wierzcholkow_pola[y1]*(wektor_wierzcholkow_pola[x2]-wektor_wierzcholkow_pola[x0]);
-    //            printf("Licznik k = %i, Indeksy:   y1 = %i, x2 = %i, x0 = %i\n",k,y1,x2,x0);
-    //            printf("                Wartosci:  %i,      %i,      %i\n",wektor_wierzcholkow_pola[y1],wektor_wierzcholkow_pola[x2],wektor_wierzcholkow_pola[x0]);
-    //            printf("Pole zadruku szare = %i\n",pole_zadruku_czarne);
-        }
-        pole1=fabs(pole_zadruku_czarne);
-        pole2=fabs(pole_zadruku_szare);
-
-        if (pole1>pole2)
-        {
-            true_pole_szare=pole1;
-            true_pole_czarne=pole2;
-        }
-        else
-        {
-            true_pole_szare=pole2;
-            true_pole_czarne=pole1;
-        }
-
-        tuszokule=((true_pole_szare-true_pole_czarne)*3+true_pole_czarne*5);
-        printf("%lld\n", tuszokule);
-        bufClear(wektor_wierzcholkow_pola);
-        dlugosc_wektora_wierzcholkow_pola=0;
-        bufClear(buforSz);
-        pole_zadruku_szare=0;
-        bufClear(buforCz);
-        pole_zadruku_czarne=0;
-    }*/
-    return 0;
 }
